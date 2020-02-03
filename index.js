57966af829abeac557e24fd755433f5487330350
helloyou2012 <helloyou2012@yeah.net>
2020-02-03 16:26:59 +0800

    update weui

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+// Core
 require('zepto/src/zepto');
 require('zepto/src/event');
 require('zepto/src/ajax');
 require('zepto/src/form');
 require('zepto/src/ie');
+// Animate
+require('zepto/src/fx');
+require('zepto/src/fx_methods');
